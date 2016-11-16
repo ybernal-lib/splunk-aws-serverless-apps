5bb122591fe838e5682d536d6efba67a41bcd149
Roy Arsan <roy.arsan@gmail.com>
2016-11-16 01:01:05 -0600

    Add IoT Button blueprint

---
+++ blueprints/splunk-iot-plugin/index.js
@@ -0,0 +1,59 @@
+/**
+ * Stream events from AWS IoT to Splunk
+ *
+ * This function streams AWS IoT Button events to Splunk using
+ * Splunk's HTTP event collector API.
+ *
+ * Follow these steps to configure this function:
+ *
+ * 1. Enter url address for your Splunk HTTP event collector endpoint.
+ * Default port for event collector is 8088. Make sure no firewalls would prevent
+ * your Lambda function from connecting to this port on your Splunk host(s).
+ *
+ * 2. Enter token for your Splunk HTTP event collector. To create a new token
+ * for this Lambda function, refer to Splunk Docs:
+ * http://docs.splunk.com/Documentation/Splunk/latest/Data/UsetheHTTPEventCollector#Create_an_Event_Collector_token
+ */
+
+var loggerConfig = {
+    url: process.env['SPLUNK_HEC_URL'] || 'https://<HOST>:<PORT>/services/collector',
+    token: process.env['SPLUNK_HEC_TOKEN'] || '<TOKEN>'
+};
+
+var SplunkLogger = require("./lib/mysplunklogger");
+var logger = new SplunkLogger(loggerConfig);
+
+/**
+ * The following JSON template shows what is sent as the payload:
+{
+    "serialNumber": "GXXXXXXXXXXXXXXXXX",
+    "batteryVoltage": "xxmV",
+    "clickType": "SINGLE" | "DOUBLE" | "LONG"
+}
+ *
+ * A "LONG" clickType is sent if the first press lasts longer than 1.5 seconds.
+ * "SINGLE" and "DOUBLE" clickType payloads are sent for short clicks.
+ *
+ * For more documentation, follow the link below.
+ * http://docs.aws.amazon.com/iot/latest/developerguide/iot-lambda-rule.html
+ */
+exports.handler = (event, context, callback) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+
+    //log JSON objects
+    logger.log(event, context);
+
+    //specify the timestamp explicitly, useful for forwarding events with embedded
+    //timestamps like from AWS IoT, AWS Kinesis, AWS CloudWatch Logs
+    logger.logWithTime(Date.now(), event, context);
+
+     //send all the events in a single batch to Splunk
+    logger.flushAsync((error, response) => {
+        if (error) {
+            callback(error);
+        } else {
+            console.log(`Hello from your IoT Button ${event.serialNumber}: ${event.clickType}`);
+            callback(null, `${event.serialNumber}:${event.clickType}`); // Return button serial number & click type
+        }
+    });
+};
