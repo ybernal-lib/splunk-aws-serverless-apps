08f6575be2385ffa0b6762e9072370db22a68357
Roy Arsan <roy.arsan@gmail.com>
2016-11-18 17:05:19 -0600

    Build script to package all blueprints

--- scripts/build-blueprint.sh
+++ scripts/build-blueprint.sh
@@ -10,29 +10,51 @@ fi
 function usage(){
   >&2 echo "
  Usage:
-    $0 blueprint-name
+    $0 version [blueprint-name]
 
  Ex:
-    $0 splunk-lambda
+    $0 1.2.3;                # Run build on all blueprints
+    $0 1.2.3 splunk-logging; # Run build on splunk-logging blueprint only
 "
   exit 1
 }
 
-if [ "$#" -lt 1 ]; then
+function build() {
+    name=$1
+    version=$2
+    package="${name}.v${version}.zip"
+
+    echo "Building package for blueprint ${name}"
+    echo ""
+    
+    pushd ${name}
+    
+    #npm install
+    zip -r ${package} index.js lambda.json lib/;
+
+    popd
+    
+    echo ""
+    echo "Package ${package} build complete"
+}
+
+if [[ "$#" -gt 2 || "$#" -lt 1 ]]; then
     usage
 fi
 
-export name=$1
+version=$1
+blueprint=$2
 
-echo "Building package for blueprint ${name}"
-echo ""
+pushd blueprints
 
-pushd blueprints/${name}
+if [ -z $blueprint ]; then
+    for dir in *; do
+        if [[ -d $dir && $dir != "lib" ]]; then
+           build $dir $version
+        fi
+    done
+else
+    build $blueprint $version
+fi
 
-npm install
-zip -r ${name}.zip index.js lib/;
-
-popd
-
-echo ""
-echo "Package ${name}.zip build complete"
+popd blueprints
