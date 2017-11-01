94bafdfa3f8e578e38d6edee892257fceb97249c
rarsan <roy.arsan@gmail.com>
2017-11-01 10:53:30 -0500

    Use splunk-logging official HEC library

--- splunk-cloudwatch-logs-processor/index.js
+++ splunk-cloudwatch-logs-processor/index.js
@@ -20,12 +20,16 @@
 const loggerConfig = {
     url: process.env.SPLUNK_HEC_URL,
     token: process.env.SPLUNK_HEC_TOKEN,
+    maxBatchCount: 0, // Manually flush events
 };
 
-const SplunkLogger = require('./lib/mysplunklogger');
+// const SplunkLogger = require('./lib/mysplunklogger');
+const SplunkLogger = require('splunk-logging').Logger;
 const zlib = require('zlib');
 
 const logger = new SplunkLogger(loggerConfig);
+// override SplunkLogger default formatter
+logger.eventFormatter = message => message;
 
 exports.handler = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
@@ -42,33 +46,30 @@ exports.handler = (event, context, callback) => {
             let count = 0;
             if (parsed.logEvents) {
                 parsed.logEvents.forEach((item) => {
-                    /* Log event to Splunk with explicit event timestamp.
-                    - Use optional 'context' argument to send Lambda metadata e.g. awsRequestId, functionName.
+                    /* Send item message to Splunk with optional metadata properties such as time, index, source, sourcetype, and host.
                     - Change "item.timestamp" below if time is specified in another field in the event.
-                    - Change to "logger.log(item.message, context)" if no time field is present in event. */
-                    logger.logWithTime(item.timestamp, item.message, context);
-
-                    /* Alternatively, UNCOMMENT logger call below if you want to override Splunk input settings */
-                    /* Log event to Splunk with any combination of explicit timestamp, index, source, sourcetype, and host.
-                    - Complete list of input settings available at http://docs.splunk.com/Documentation/Splunk/latest/RESTREF/RESTinput#services.2Fcollector */
-                    // logger.logEvent({
-                    //     time: new Date(item.timestamp).getTime() / 1000,
-                    //     host: 'serverless',
-                    //     source: `lambda:${context.functionName}`,
-                    //     sourcetype: 'httpevent',
-                    //     index: 'main',
-                    //     event: item.message,
-                    // });
+                    - Set or remove metadata properties as needed. For descripion of each property, refer to:
+                    http://docs.splunk.com/Documentation/Splunk/latest/RESTREF/RESTinput#services.2Fcollector */
+                    logger.send({
+                        message: item.message,
+                        metadata: {
+                            time: item.timestamp ? new Date(item.timestamp).getTime() / 1000 : Date.now(),
+                            host: 'serverless',
+                            source: `lambda:${context.functionName}`,
+                            sourcetype: 'httpevent',
+                            index: 'sandbox',
+                        },
+                    });
 
                     count += 1;
                 });
             }
             // Send all the events in a single batch to Splunk
-            logger.flushAsync((error, response) => {
+            logger.flush((error, response, body) => {
                 if (error) {
                     callback(error);
                 } else {
-                    console.log(`Response from Splunk:\n${response}`);
+                    console.log('Response from Splunk:', body);
                     console.log(`Successfully processed ${count} log event(s).`);
                     callback(null, count); // Return number of log events
                 }
