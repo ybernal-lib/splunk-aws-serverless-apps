69dddcd846c7cc0a5d22ea9aaeff2f72268fdbf6
rarsan <roy.arsan@gmail.com>
2017-11-13 11:12:37 -0600

    Refactor generic splunk-logging to use Splunk HEC client library

--- splunk-logging/index.js
+++ splunk-logging/index.js
@@ -19,49 +19,77 @@
 const loggerConfig = {
     url: process.env.SPLUNK_HEC_URL,
     token: process.env.SPLUNK_HEC_TOKEN,
+    maxBatchCount: 0, // Manually flush events
+    maxRetries: 3,    // Retry 3 times
 };
 
-const SplunkLogger = require('./lib/mysplunklogger');
+const SplunkLogger = require('splunk-logging').Logger;
 
 const logger = new SplunkLogger(loggerConfig);
 
 exports.handler = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
 
-    // Log JSON objects to Splunk
-    logger.log(event);
+    // First, configure logger to automatically add Lambda metadata and to hook into Lambda callback
+    configureLogger(context, callback); // eslint-disable-line no-use-before-define
 
-    // Log JSON objects with optional 'context' argument (recommended)
-    // This adds valuable Lambda metadata including functionName as source, awsRequestId as field
-    logger.log(event, context);
+    // Log JSON objects to Splunk
+    logger.send({ message: event });
 
     // Log strings
-    logger.log(`value1 = ${event.key1}`, context);
+    logger.send({ message: `value1 = ${event.key1}` });
 
-    // Log with user-specified timestamp - useful for forwarding events with embedded
-    // timestamps, such as from AWS IoT, AWS Kinesis, AWS CloudWatch Logs
+    // Log object or string with explicit timestamp - useful for forwarding events with embedded
+    // timestamps, such as from AWS IoT, AWS Kinesis Stream & Firehose, AWS CloudWatch Logs
     // Change "Date.now()" below to event timestamp if specified in event payload
-    logger.logWithTime(Date.now(), event, context);
+    logger.send({
+        message: event,
+        metadata: {
+            time: Date.now(),
+        },
+    });
 
-    // Advanced:
-    // Log event with user-specified request parameters - useful to set input settings per event vs token-level
-    // Full list of request parameters available here:
-    // http://docs.splunk.com/Documentation/Splunk/latest/RESTREF/RESTinput#services.2Fcollector
-    logger.logEvent({
-        time: Date.now(),
-        host: 'serverless',
-        source: `lambda:${context.functionName}`,
-        sourcetype: 'httpevent',
-        event: event,
+    // Log object or string with optional metadata parameters - useful to set input settings per event vs HEC token-level
+    // For descripion of each metadata parameter, refer to:
+    // http://docs.splunk.com/Documentation/Splunk/latest/RESTREF/RESTinput#services.2Fcollector */
+    logger.send({
+        message: event,
+        metadata: {
+            time: Date.now(),
+            host: 'serverless',
+            source: `lambda:${context.functionName}`,
+            sourcetype: 'httpevent',
+            //index: 'main',
+        },
     });
 
     // Send all the events in a single batch to Splunk
-    logger.flushAsync((error, response) => {
-        if (error) {
-            callback(error);
+    logger.flush((err, resp, body) => {
+        // Request failure or valid response from Splunk with HEC error code
+        if (err || (body && body.code !== 0)) {
+            // If failed, error will be handled by pre-configured logger.error() below
         } else {
-            console.log(`Response from Splunk:\n${response}`);
+            // If succeeded, body will be { text: 'Success', code: 0 }
+            console.log('Response from Splunk:', body);
             callback(null, event.key1); // Echo back the first key value
         }
     });
+};
+
+const configureLogger = (context, callback) => {
+    // Override SplunkLogger default formatter
+    logger.eventFormatter = (event) => {
+        // Enrich event only if it is an object
+        if (typeof event === 'object' && !Object.prototype.hasOwnProperty.call(event, 'awsRequestId')) {
+            // Add awsRequestId from Lambda context for request tracing
+            event.awsRequestId = context.awsRequestId; // eslint-disable-line no-param-reassign
+        }
+        return event;
+    };
+
+    // Set common error handler for logger.send() and logger.flush()
+    logger.error = (error, payload) => {
+        console.log('error', error, 'context', payload);
+        callback(error);
+    };
 };
